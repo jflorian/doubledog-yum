a49874aac46820f8ba2401a441f6bc7de6751564
John Florian <jflorian@doubledog.org>
2012-04-17 18:36:10 -0400

    New - yum::remove definition and related refactoring
    It was desired to make the package absence of the yum-presto and
    PackageKit-yum-plugin packages part of yum::core where they seemed to be
    a better fit.  This was largely desired because the very initial runs of
    puppet might try to use the delta-rpm support provided by yum-presto.
    Since yum::core is a dependency of all Package resources, it was thought
    that this would be a good way to accomplish that.
    
    However, that set up a circular dependency, so I went to change those to
    Package resources to Exec wrappers around 'yum remove' and then realized
    that the code base was becoming littered with these and that a new
    definition was in order, hence yum::remove.
    
    This contains all of that, plus the transition of other existing
    Exec['yum remove'] instances to use the new yum::remove definition.
    
    I *think* this is better, but it's still fugly.  Nonetheless, it should
    make repairs easier should a better solution come along.

---
+++ manifests/definitions/remove.pp
@@ -0,0 +1,37 @@
+# modules/yum/manifests/definitions/remove.pp
+#
+# Synopsis:
+#       Remove a package directly with yum.
+#
+# Parameters:
+#       Name__________  Default_______  Description___________________________
+#
+#       name                            package name
+#
+# Requires:
+#       NONE
+#
+# Notes:
+#       Generally it would be preferred to use puppet's native Package
+#       resource to ensure a package absence, but there are situations where
+#       that doesn't work well, such as when:
+#               1. the package may have a dependency chain that would be
+#               difficult to express using native Package resources.
+#               2. during the configuration of YUM itself (see
+#               Class['yum::config']) where this would set up a dependency
+#               loop between Packages and that class.
+#
+# Example usage:
+#
+#       include yum
+#
+#       yum::remove { 'foo': }
+
+
+define yum::remove {
+
+    exec { "yum -y remove ${name}":
+        onlyif  => "rpm -q ${name}",
+    }
+
+}
