6da6ffa00cb22a54801c3160955aa53d96aaaa20
John Florian <jflorian@doubledog.org>
2012-07-30 19:50:17 -0400

    Drop - yum/mirror support for EoL Fedoras

--- manifests/doubledog.pp
+++ manifests/doubledog.pp
@@ -19,10 +19,6 @@ class yum::doubledog {
         server_uri  => "http://www.doubledog.org/yum/fedora/${operatingsystemrelease}/${architecture}",
         pkg_name    => "doubledog-yum-repo",
         pkg_release => $operatingsystemrelease ? {
-            "11"        => "10-1.fc10.noarch",
-            "12"        => "12-1.fc12.noarch",
-            "13"        => "13-2.fc13.noarch",
-            "14"        => "14-1.fc14.noarch",
             "15"        => "15-1.fc15.noarch",
             "16"        => "16-1.fc16.noarch",
         },
--- manifests/local-fedora.pp
+++ manifests/local-fedora.pp
@@ -19,10 +19,6 @@ class yum::local-fedora {
         server_uri  => "http://www.doubledog.org/yum/fedora/${operatingsystemrelease}/${architecture}",
         pkg_name    => "yum-local-mirror-conf",
         pkg_release => $operatingsystemrelease ? {
-            "11"        => "10-1.fc10.noarch",
-            "12"        => "10-1.fc10.noarch",
-            "13"        => "13-1.fc13.noarch",
-            "14"        => "14-1.fc14.noarch",
             "15"        => "15-1.fc15.noarch",
             "16"        => "16-1.fc15.noarch",
         },
